801b0b955b784e798b9e099b8eb2d1640ce2a602
Matthew Day <Daystar.1998@yahoo.com>
2019-09-16 20:42:24 -0500

    Add readBinaryFile

--- Hex Dump/Hex Dump/src/hexDump.cpp
+++ Hex Dump/Hex Dump/src/hexDump.cpp
@@ -9,10 +9,48 @@
 	Due Date: 9/20/2019
 ******************************************************************************/
 
+#include <fstream>
 #include<iostream>
 #include<string>
 
 using namespace std;
+
+/******************************************************************************
+		Name: readBinaryFile
+
+		Des:
+			Reads data from a binary file
+
+		Params:
+			fileName - type string &, the name of the file
+
+		Return:
+			type unsigned char *, the binary data
+
+******************************************************************************/
+unsigned char *readBinaryFile(string &fileName) {
+
+	unsigned char *buffer;
+
+	ifstream fin(fileName, ios::in | ios::binary | ios::ate);
+
+	if (fin.is_open()) {
+
+		int sizeOfFile = (int)fin.tellg();
+		buffer = new unsigned char[sizeOfFile];
+
+		fin.seekg(0, ios::beg);
+		fin.read((char *)buffer, sizeOfFile);
+
+		fin.close();
+	} else {
+
+		cout << "Error: Unable to open file: " << fileName << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	return buffer;
+}
 
 int main() {
 
@@ -22,6 +60,8 @@ int main() {
 
 		getline(cin, fileName);
 	} while (fileName == "");
+
+	unsigned char *buffer = readBinaryFile(fileName);
 
 	return 0;
 }
